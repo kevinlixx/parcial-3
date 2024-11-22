34919389125a2c4754d806c7f75e1ec7a3fe4269
Kevin Lis <113084403+kevinlixx@users.noreply.github.com>
2024-11-21 22:00:41 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/parcial-3/', // Reemplaza 'proyectoidx2' con el nombre de tu repositorio
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
 });
